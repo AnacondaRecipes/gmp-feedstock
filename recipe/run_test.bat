558b413bfb2cda40573ac268e147dc183422ed71
Serhii Kupriienko <skupriienko@anaconda.com>
2024-01-25 10:12:18 +0200

    Fix the tests

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -5,8 +5,10 @@ if not exist %PREFIX%\Library\mingw-w64\bin\libgmpxx-4.dll exit /b 1
 if not exist %PREFIX%\Library\mingw-w64\include\gmp.h exit /b 1
 if not exist %PREFIX%\Library\mingw-w64\lib\libgmp.a exit /b 1
 
+set "PATH=%PREFIX%\lib;%PATH%"
+
 REM Build the test.c program
-%CC% -I %PREFIX%\include -L %PREFIX%\lib test.c -lgmp -Wl,-rpath,%PREFIX%\lib -o test.exe
+%CC% /I %PREFIX%\include /LIBPATH:%PREFIX%\lib test.c -lgmp -o test_gmp.exe
 
 REM Run the compiled program
-test.exe
+test_gmp.exe
