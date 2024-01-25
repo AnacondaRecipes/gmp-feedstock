5f5afa3384de6a13cb2f13474a12d7388c2707e1
Serhii Kupriienko <skupriienko@anaconda.com>
2024-01-25 12:36:16 +0200

    Fix static libraries, fix paths

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,14 +1,18 @@
 @echo on
 
-if not exist %PREFIX%\Library\mingw-w64\bin\libgmp-10.dll exit /b 1
-if not exist %PREFIX%\Library\mingw-w64\bin\libgmpxx-4.dll exit /b 1
-if not exist %PREFIX%\Library\mingw-w64\include\gmp.h exit /b 1
-if not exist %PREFIX%\Library\mingw-w64\lib\libgmp.a exit /b 1
+dir \s
 
-set "PATH=%PREFIX%\Library\mingw-w64\lib;%PATH%"
+if not exist %LIBRARY_PREFIX%\mingw-w64\bin\libgmp-10.dll exit /b 1
+if not exist %LIBRARY_PREFIX%\mingw-w64\bin\libgmpxx-4.dll exit /b 1
+if not exist %LIBRARY_PREFIX%\mingw-w64\include\gmp.h exit /b 1
+::if not exist %PREFIX%\Library\mingw-w64\lib\libgmp.a exit /b 1
+if not exist %LIBRARY_PREFIX%\mingw-w64\lib\libgmp.lib exit /b 1
+if not exist %LIBRARY_PREFIX%\mingw-w64\lib\libgmp_static.lib exit /b 1
 
-REM Build the test.c program
-%CC% /I %PREFIX%\Library\mingw-w64\include /LIBPATH:%PREFIX%\Library\mingw-w64\lib test.c -lgmp -o test.exe
+::set PATH=%LIBRARY_PATH%\mingw-w64\bin;%LIBRARY_PREFIX%\usr\bin;%LIBRARY_BIN%;%PATH%
 
-REM Run the compiled program
+%CC% /Tc test.c /I %CONDA_PREFIX%\Library\mingw-w64\include /link /LIBPATH:"%CONDA_PREFIX%\Library\mingw-w64\lib" libgmp.lib /out:test.exe
+
+if errorlevel 1 exit /B 1
 test.exe
+if errorlevel 1 exit /B 1
