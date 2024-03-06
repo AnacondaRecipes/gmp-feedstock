91f1175becaf30ad9ecaf9a55b17587bd1347f01
Serhii Kupriienko <skupriienko@anaconda.com>
2024-03-06 10:55:58 +0200

    Update paths, move files to LIBRARY

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,23 +6,23 @@ set -e
 cp $BUILD_PREFIX/share/gnuconfig/config.guess config.fsf.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.sub config.fsf.sub
 
-IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
+IFS=$' \t\n'
 
 # Adopt a Unix-friendly path if we're on Windows (see bld.bat).
 [ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
 
 # On Windows we want $LIBRARY_PREFIX in both "mixed" (C:/Conda/...) and Unix
 # (/c/Conda) forms, but Unix form is often "/" which can cause problems.
-if [ -n "$LIBRARY_PREFIX_M" ] ; then
-    mprefix="$LIBRARY_PREFIX_M"
-    if [ "$LIBRARY_PREFIX_U" = / ] ; then
-        uprefix=""
+if [ -n "$LIBRARY_PREFIX_MIXED" ] ; then
+    mixed_prefix="$LIBRARY_PREFIX_MIXED"
+    if [ "$LIBRARY_PREFIX_UNIX" = / ] ; then
+        unix_prefix=""
     else
-        uprefix="$LIBRARY_PREFIX_U"
+        unix_prefix="$LIBRARY_PREFIX_UNIX"
     fi
 else
-    mprefix="$PREFIX"
-    uprefix="$PREFIX"
+    mixed_prefix="$PREFIX"
+    unix_prefix="$PREFIX"
 fi
 
 # On Windows we need to regenerate the configure scripts.
@@ -33,13 +33,13 @@ if [ -n "$CYGWIN_PREFIX" ] ; then
     autoreconf_args=(
         --force
         --install
-        -I "$mprefix/share/aclocal"
-        -I "$BUILD_PREFIX_M/Library/mingw-w64/share/aclocal"
+        -I "$mixed_prefix/share/aclocal"
+        -I "$BUILD_PREFIX_MIXED/Library/mingw-w64/share/aclocal"
     )
     autoreconf "${autoreconf_args[@]}"
 fi
 
-export PKG_CONFIG_LIBDIR=$uprefix/lib/pkgconfig:$uprefix/share/pkgconfig
+export PKG_CONFIG_LIBDIR=$unix_prefix/lib/pkgconfig:$unix_prefix/share/pkgconfig
 
 mkdir build
 cd build
@@ -58,8 +58,16 @@ make install
 
 
 if [[ "$target_platform" != win* ]] ; then
-    # This overlaps with libgcc-ng: 
+    # This overlaps with libgcc-ng:
     rm -rf "${PREFIX}"/share/info/dir
+    
+    # Move files to LIBRARY_LIB
+    mv "${PREFIX}"/Library/mingw-w64/bin/libgmp-10.dll "${PREFIX}"/Library/bin/libgmp-10.dll
+    mv "${PREFIX}"/Library/mingw-w64/bin/libgmpxx-4.dll "${PREFIX}"/Library/bin/libgmpxx-4.dll
+    mv "${PREFIX}"/Library/mingw-w64/include/gmp.h "${PREFIX}"/Library/include/gmp.h
+    mv "${PREFIX}"/Library/mingw-w64/lib/libgmp.lib "${PREFIX}"/Library/lib/libgmp.lib
+    mv "${PREFIX}"/Library/mingw-w64/lib/libgmp_static.lib "${PREFIX}"/Library/lib/libgmp_static.lib
+    
 else
     # Move the static library
     mv "${PREFIX}"/Library/mingw-w64/lib/libgmp.a "${PREFIX}"/lib/libgmp_static.lib
--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -2,15 +2,19 @@
 
 dir /s %PREFIX%\Library\
 
+if not exist %PREFIX%\Library\bin\libgmp-10.dll exit /b 1
+if not exist %PREFIX%\Library\bin\libgmpxx-4.dll exit /b 1
+if not exist %PREFIX%\Library\include\gmp.h exit /b 1
+if not exist %PREFIX%\Library\lib\libgmp.lib exit /b 1
+if not exist %PREFIX%\Library\lib\libgmp_static.lib exit /b 1
 if not exist %PREFIX%\Library\mingw-w64\bin\libgmp-10.dll exit /b 1
 if not exist %PREFIX%\Library\mingw-w64\bin\libgmpxx-4.dll exit /b 1
 if not exist %PREFIX%\Library\mingw-w64\include\gmp.h exit /b 1
-::if not exist %PREFIX%\Library\mingw-w64\lib\libgmp.a exit /b 1
 if not exist %PREFIX%\Library\mingw-w64\lib\libgmp.lib exit /b 1
 if not exist %PREFIX%\Library\mingw-w64\lib\libgmp_static.lib exit /b 1
 
 
-%CC% /Tc test.c /I %CONDA_PREFIX%\Library\mingw-w64\include /link /LIBPATH:"%CONDA_PREFIX%\Library\mingw-w64\lib" libgmp.lib /out:test.exe
+%CC% /Tc test.c /I %CONDA_PREFIX%\Library\include /link /LIBPATH:"%CONDA_PREFIX%\Library\lib" libgmp.lib /out:test.exe
 
 if errorlevel 1 exit /B 1
 test.exe
