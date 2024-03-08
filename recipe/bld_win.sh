8e2ddaff4b26ff2d0a9c661492e7415d18ed13c5
Serhii Kupriienko <skupriienko@anaconda.com>
2024-03-08 15:35:56 +0200

    Fix copying files

--- recipe/bld_win.sh
+++ recipe/bld_win.sh
@@ -14,13 +14,13 @@ make check
 make install
 
 # Move the static libraries
-cp "${PREFIX}"/lib/libgmp.a "${PREFIX}"/Library/lib/libgmp_static.lib
-cp "${PREFIX}"/lib/libgmpxx.a "${PREFIX}"/Library/lib/libgmpxx_static.lib
+cp "${PREFIX}"/lib/libgmp.a "${PREFIX}"/lib/libgmp_static.lib
+cp "${PREFIX}"/lib/libgmpxx.a "${PREFIX}"/lib/libgmpxx_static.lib
 # # Move header files
 cp "${PREFIX}"/include/gmp.h "${PREFIX}"/Library/include/gmp.h
 cp "${PREFIX}"/include/gmpxx.h "${PREFIX}"/Library/include/gmpxx.h
 # Move the pkg-config file
-cp "${PREFIX}"/lib/pkgconfig/gmp.pc "${PREFIX}"/Library/lib/pkgconfig/gmp.pc
+#cp "${PREFIX}"/lib/pkgconfig/gmp.pc "${PREFIX}"/Library/lib/pkgconfig/gmp.pc
 
 ls -l
 
@@ -39,10 +39,10 @@ make install
 ls -l
 
 # Move import libraries
-cp "${PREFIX}"/lib/libgmp.dll.a "${PREFIX}"/Library/lib/libgmp.lib
-cp "${PREFIX}"/lib/libgmpxx.dll.a "${PREFIX}"/Library/lib/libgmpxx.lib
+cp "${PREFIX}"/lib/libgmp.dll.a "${PREFIX}"/lib/libgmp.lib
+cp "${PREFIX}"/lib/libgmpxx.dll.a "${PREFIX}"/Library/libgmpxx.lib
 # Move dynamic libraries
-cp "${PREFIX}"/bin/libgmp-10.dll "${PREFIX}"/Library/bin/libgmp-10.dll
-cp "${PREFIX}"/bin/libgmpxx-4.dll "${PREFIX}"/Library/bin/libgmpxx-4.dll
+#cp "${PREFIX}"/bin/libgmp-10.dll "${PREFIX}"/Library/bin/libgmp-10.dll
+#cp "${PREFIX}"/bin/libgmpxx-4.dll "${PREFIX}"/Library/bin/libgmpxx-4.dll
 
 find $PREFIX -name '*.la' -delete
