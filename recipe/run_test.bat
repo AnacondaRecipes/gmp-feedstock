e57e3518a64d34fc6cd492629219dfb6ecdc8dc3
Serhii Kupriienko <skupriienko@anaconda.com>
2024-03-12 10:50:29 +0200

    Remove comments, compile tests only on unix

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,28 +1,11 @@
 @echo on
 
 REM static libraries:
-if not exist %PREFIX%\lib\libgmp.lib exit /b 1
-if not exist %PREFIX%\lib\libgmpxx.lib exit /b 1
-if not exist %PREFIX%\lib\libgmp_static.lib exit /b 1
-if not exist %PREFIX%\lib\libgmpxx_static.lib exit /b 1
-REM import libraries:
-if not exist %PREFIX%\lib\libgmp.lib exit /b 1
-if not exist %PREFIX%\lib\libgmpxx.lib exit /b 1
+if not exist %PREFIX%\lib\libgmp.a exit /b 1
+if not exist %PREFIX%\lib\libgmpxx.a exit /b 1
 REM dynamic libraries:
 if not exist %PREFIX%\bin\libgmp-10.dll exit /b 1
 if not exist %PREFIX%\bin\libgmpxx-4.dll exit /b 1
 REM headers:
 if not exist %PREFIX%\include\gmp.h exit /b 1
 if not exist %PREFIX%\include\gmpxx.h exit /b 1
-
-REM compile a file with an import library
-%CC% /MD /Fetest test.c /I%PREFIX%\include /link /LIBPATH:"%PREFIX%\lib" libgmp.lib
-
-REM Uncomment for the debugging purposes only to check the dynamic libraries
-ldd test.exe
-if errorlevel 1 exit /B 1
-
-if errorlevel 1 exit /B 1
-REM execute the output
-test.exe
-if errorlevel 1 exit /B 1
