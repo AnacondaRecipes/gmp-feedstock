249969630ae2adb2df16cdd2e9d1199ccc1d9ef9
Serhii Kupriienko <skupriienko@anaconda.com>
2024-03-12 11:47:13 +0200

    Remove the static library, enable make check

--- recipe/bld_win.sh
+++ recipe/bld_win.sh
@@ -8,28 +8,12 @@ export BUILD=${HOST}
 
 export CFLAGS="${CFLAGS} -Wno-attributes -Wno-ignored-attributes"
 
-# Build static and shared libraries separately because `make` can build only one at a time
-mkdir -p "static" && cd "static"
-
-../configure --prefix="${PREFIX}" --host="${HOST}" --enable-cxx --enable-fat --enable-static --disable-shared
-
-make -j${CPU_COUNT}
-#make check -j${CPU_COUNT}
-make install
-
-
-find $PREFIX -name '*.la' -delete
-
-cd ..
-
-
+# Build shared libraries.
+# To be aware: 'make' can build only one library at a time: shared or static but not both.
 mkdir -p "shared" && cd "shared"
 
 ../configure --prefix="${PREFIX}" --host="${HOST}" --enable-cxx --enable-fat --enable-shared --disable-static
 
 make -j${CPU_COUNT}
-#make check -j${CPU_COUNT}
+make check -j${CPU_COUNT}
 make install
-
-
-find $PREFIX -name '*.la' -delete
--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -9,3 +9,10 @@ if not exist %PREFIX%\bin\libgmpxx-4.dll exit /b 1
 REM headers:
 if not exist %PREFIX%\include\gmp.h exit /b 1
 if not exist %PREFIX%\include\gmpxx.h exit /b 1
+
+REM compile a file with an import library
+%CC% /MD /Fetest test.c /I%PREFIX%\include /link /LIBPATH:"%PREFIX%\lib"
+
+REM execute the output
+test.exe
+if errorlevel 1 exit /B 1
